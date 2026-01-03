b9acce8f57dab813ef451f03d0514538095912bd
Hadia8602 <hadiaishaq932@gmail.com>
2026-01-03 23:50:29 +0500

    adding main files

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+switch(choice) {
+    case 1: pharmacyMenu(); break;
+    case 2: staffMenu(); break;        // <-- This is your Staff menu
+    case 3: labtestMenu() break;
+    case 4: billingMenu() break;
+    case 5: patientMenu() break;
+    case 6: cout << "Exiting Program...\n"; break;
+    default: cout << "Invalid Choice\n";
+}
