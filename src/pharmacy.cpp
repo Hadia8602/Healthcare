51dfdd3b492b8f77899c3a20b327fc8b297c66dd
Hadia8602 <hadiaishaq932@gmail.com>
2026-01-03 21:19:37 +0500

    ading the delete code in pharmacy

--- src/pharmacy.cpp
+++ src/pharmacy.cpp
@@ -68,3 +68,37 @@ void edit() {
         cout << "invalid name!\n";
 }
 
+void del() {
+    string line, name_to_del, noOfTablets, expiry_date, mg, name;
+    cout << "Enter the name of medicine you want to delete: ";
+    getline(cin, name_to_del);
+
+    bool found = false;
+    ifstream file("pharmacy.csv");
+    ofstream temp("temp.csv");
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        getline(ss, noOfTablets, ',');
+        getline(ss, name, ',');
+        getline(ss, mg, ',');
+        getline(ss, expiry_date, ',');
+        if (name_to_del == name) {
+            found = true;
+            continue;
+        }
+        temp << line << "\n";
+    }
+
+    file.close();
+    temp.close();
+    remove("pharmacy.csv");
+    rename("temp.csv", "pharmacy.csv");
+
+    if (found)
+        cout << "medicine deleted successfully\n";
+    else
+        cout << "invalid name!\n";
+}
+
+
